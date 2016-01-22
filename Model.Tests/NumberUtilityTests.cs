356b916f28fa52962fc84ab4c3951a6a52662c99
aadennis <dennis.circ@gmail.com>
2016-01-22 22:38:33 +0000

    Unit tests of number utilities actually doing something now
    Unit tests of number utilities actually doing something now

--- Model.Tests/NumberUtilityTests.cs
+++ Model.Tests/NumberUtilityTests.cs
@@ -1,12 +1,16 @@
 using Xunit;
-using Model;
 using System;
 
 namespace Model.Tests {
     
+
+    
     public class NumberUtilityTests {
         
+        const int MaxIterationsToTestForError = 1000;
+        
         Model.NumberUtilities util;
+        Random rand;
         
         public NumberUtilityTests() {
             TestInitialize();
@@ -14,11 +18,45 @@ namespace Model.Tests {
         
         [Fact]
         public void UpperAndLowerNumbersAreNeverTheSame() {
-            Assert.Equal(1,2);
+           for (int i = 0; i < MaxIterationsToTestForError; i++) {
+               var randomBoundaries = util.GetRandomInterval(1,30,7,rand);
+               Assert.NotEqual(randomBoundaries[0],randomBoundaries[1]);
+           }
         }
-
+        
+        [Fact]
+        public void UpperAndLowerNumbersAreAlwaysWithinRequestedDistance() {
+           const int maxDistance = 7;
+           for (int i = 0; i < MaxIterationsToTestForError; i++) {
+               var randomBoundaries = util.GetRandomInterval(1,30,7,rand);
+            //    System.Console.WriteLine(randomBoundaries[0]);
+            //    System.Console.WriteLine(randomBoundaries[1]);
+            //    System.Console.WriteLine("-----------------");
+               Assert.True(maxDistance >= (randomBoundaries[1] - randomBoundaries[0]));
+           }
+        }
+        
+         [Fact]
+         public void ReturnInputIfRequestedUpperAndLowerLimitsMatch() {
+           var upperLimit = 7;
+           var lowerLimit = upperLimit;
+           var randomBoundaries = util.GetRandomInterval(lowerLimit,upperLimit,70000,rand);
+           Assert.Equal(randomBoundaries[0],randomBoundaries[1]);
+        }
+        
         private void TestInitialize() {
           util = new Model.NumberUtilities();
+          rand = new Random();
         }
     }
 }
+
+// Example execution:
+//     PS C:\temp\ps\Tones\Tones> dnx -p .\Model.Tests\project.json test
+// xUnit.net DNX Runner (64-bit DNXCore 5.0)
+//   Discovering: Model.Tests
+//   Discovered:  Model.Tests
+//   Starting:    Model.Tests
+//   Finished:    Model.Tests
+// === TEST EXECUTION SUMMARY ===
+//    Model.Tests  Total: 3, Errors: 0, Failed: 0, Skipped: 0, Time: 0.164s
--- Model/NumberUtilities.cs
+++ Model/NumberUtilities.cs
@@ -13,9 +13,18 @@ namespace Model {
         ///  It helps if the Random passed in has been used in previous calls, to help entropy.
         /// </summary>
         public List<int> GetRandomInterval(int lowerLimit, int upperLimit, int maxDistance, Random rand) {
+            if (lowerLimit.Equals(upperLimit)) {
+                return new List<int> {lowerLimit, upperLimit};
+            }
+            
+            const int MaxIterations = 100;
             var lowerAndUpperLimit = new List<int>{rand.Next(lowerLimit,upperLimit)};
             var nextNote = rand.Next(lowerLimit, upperLimit);
+            var count = 0;
             while (lowerAndUpperLimit[0] == nextNote || System.Math.Abs(lowerAndUpperLimit[0] - nextNote) > maxDistance) {
+                if (count++ > MaxIterations) {
+                    throw new Exception("Too many iterations");
+                }
                 nextNote = rand.Next(lowerLimit, upperLimit);
             }
             lowerAndUpperLimit.Add(nextNote);
